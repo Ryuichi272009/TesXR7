a6bd885d45567be21ed4322d2ed4719e9ef3d5ae
Ryuichi272009 <ryuichi_diawan_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 13:54:04 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,7 @@
+public class Tes {
+    public static void main(String[] args ) {
+        System.out.println("Nama saya Ryuichi hikaru diawan dari kelas X RPL 7 yang berasal dari kota lamongan");
+        String a = "1";
+
+    }
+}
