bc8ccddccd5c4731ebdf3f2743d51fc6bb171c75
Ryuichi272009 <ryuichi_diawan_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:38:00 +0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,18 @@
+public class Data {
+    public static void main(String[] args) {
+        //tipe data nama_var = value/isi var;
+    String nama = "Ryuichi";
+    int usia = 15;
+    String alamat = "Jl.Danau ranau l";
+    double kelas = 10.7;
+
+    System.out.println("Nama saya : "+ nama);
+System.out.println("Usia : " + usia + " tahun");
+System.out.println("Alamat : " + alamat);
+System.out.println("Kelas : " + kelas);
+
+ 
+
+    }
+}
+
